a037e97d24b3df86689ea81f78c3458cde5192d6
Felipe Reis <134147645+felipereis13@users.noreply.github.com>
2024-09-19 21:56:08 +0000

    atividade até a questão 06

--- aula.js
+++ aula.js
@@ -1,10 +1,10 @@
-//concatenacao de String
+//Questão 01
 /*var nome = "Carol";
 var idade = "25";
 
 console.log(`Olá! Meu nome é ${nome}, e tenho ${idade} anos.`);*/
 
-//inputs
+/*inputs
 const prompt = require("prompt-sync")()
 
 var n1 = prompt("insira a nota 1: ")
@@ -13,4 +13,46 @@ var n3 = 5
 
 var media = (n1 + n2 + n3) / 3
 var resultado = media.toFixed(2)
-console.log(`Sua média é... ${resultado}.`)
+console.log(`Sua média é... ${resultado}.`)*/
+
+//Questão 02
+/*var nome = "Recife"
+var cidade = "Pernambuco"
+console.log(`${nome} é de ${cidade}.`)*/
+
+//Questão 03
+/*var num1 = 10;
+var num2 = 100;
+
+var soma = (num1 + num2)
+var subrtracao = num1 - num2
+var multiplicacao = num1 * num2
+var devisao = num2 / num1
+
+console.log(`A soma é igual ${soma}, a subtração é igual a ${subrtracao}, a multiplicação é igual a ${multiplicacao} e a divisão é igual a ${devisao}.`);*/
+
+//Questão 04
+/*var base = 8
+var altura = 12
+var area = ((base * altura) / 2)
+console.log(`A área do triangulo é ${area}`)*/
+
+//Questão 5
+/*var n1 = 8;
+var n2 = 2;
+var n3 = 9;
+var media = ((n1 + n2 + n3) / 3);
+var resultado = media.toFixed(1)
+console.log(`O resultado da média é ${resultado}`);*/
+
+//Questão 06
+/*
+var valor = 1000;
+var percentualDesconto = 20;
+var desconto = ((valor * percentualDesconto) / 100);
+var total = (valor - desconto);
+var valorFinal = total.toFixed(2);
+console.log(`Valor final com desconto é ${valorFinal}`); */
+
+//Questão 07
+
