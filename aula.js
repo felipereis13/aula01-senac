6d288f562160045d1470c05e4015370f79d4f737
Felipe Reis <134147645+felipereis13@users.noreply.github.com>
2024-09-19 00:01:43 +0000

    commit de ajuste

---
+++ aula.js
@@ -0,0 +1,4 @@
+var nome = "Carol";
+var idade = "25";
+
+console.log(`Olá! Meu nome é ${nome}, e tenho ${idade} anos.`);
--- tipoPrimitivos.js
+++ tipoPrimitivos.js
@@ -2,4 +2,8 @@ rotulo = "Natalya" //Declarando a variável
 
 
 rotulo = 12345 //Retribuindo uma variável
+<<<<<<< HEAD
 console.log(rotulo);
+=======
+console.log(rotulo);
+>>>>>>> 084bc54 (commit de ajuste)
