0993c6196ccacdd3da50e5476db9ff7b79fc901b
Felipe Reis <134147645+felipereis13@users.noreply.github.com>
2024-09-19 17:59:45 +0000

    reparo

--- tipoPrimitivos.js
+++
@@ -1,9 +0,0 @@
-rotulo = "Natalya" //Declarando a variável
-
-
-rotulo = 12345 //Retribuindo uma variável
-<<<<<<< HEAD
-console.log(rotulo);
-=======
-console.log(rotulo);
->>>>>>> 084bc54 (commit de ajuste)
