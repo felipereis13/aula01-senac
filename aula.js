c71eec9c55edb8f0df8f834f766fd60f879c7ba2
Felipe Reis <134147645+felipereis13@users.noreply.github.com>
2024-09-19 00:43:52 +0000

    commit aula

--- aula.js
+++ aula.js
@@ -1,4 +1,16 @@
-var nome = "Carol";
+//concatenacao de String
+/*var nome = "Carol";
 var idade = "25";
 
-console.log(`Olá! Meu nome é ${nome}, e tenho ${idade} anos.`);
+console.log(`Olá! Meu nome é ${nome}, e tenho ${idade} anos.`);*/
+
+//inputs
+const prompt = require("prompt-sync")()
+
+var n1 = prompt("insira a nota 1: ")
+var n2 = 9
+var n3 = 5
+
+var media = (n1 + n2 + n3) / 3
+var resultado = media.toFixed(2)
+console.log(`Sua média é... ${resultado}.`)
